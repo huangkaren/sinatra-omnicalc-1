58bcbc35d912c31cb0fa3f3cbae263a02a520974
huangkaren <karenhuang@uchicago.edu>
2024-10-21 15:58:10 +0000

    square root

--- app.rb
+++ app.rb
@@ -7,3 +7,25 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_i
+  @the_result = (@the_num ** 2).to_f
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_i
+  @the_result = (@the_num ** 1/2).to_f
+
+  erb(:square_root_results)
+end
